81bcc02a4c566ebbab7908e1bf8792f0745a9863
SODANIKS <hjkpftyi@gmail.com>
2024-08-14 12:38:00 +0300

    Initial commit

---
+++ lua/autorun/roro_police.lua
@@ -0,0 +1,41 @@
+-------------------------[ Male ]-------------------------
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #1", "models/roro/police_male_1.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #1", "models/roro/police_male_1.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #1", "models/roro/arms/c_arms_police_1.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #2", "models/roro/police_male_2.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #2", "models/roro/police_male_2.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #2", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #3", "models/roro/police_male_3.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #3", "models/roro/police_male_3.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #1", "models/roro/arms/c_arms_police_1.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #4", "models/roro/police_male_4.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #4", "models/roro/police_male_4.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #4", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #5", "models/roro/police_male_5.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #5", "models/roro/police_male_5.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #5", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #6", "models/roro/police_male_6.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #6", "models/roro/police_male_6.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #6", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #7", "models/roro/police_male_7.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #7", "models/roro/police_male_7.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #7", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #8", "models/roro/police_male_8.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #8", "models/roro/police_male_8.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #8", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #9", "models/roro/police_male_9.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #9", "models/roro/police_male_9.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #9", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
+
+list.Set( "PlayerOptionsModel", "Police Roro - Male #10", "models/roro/police_male_10.mdl" )
+player_manager.AddValidModel( "Police Roro - Male #10", "models/roro/police_male_10.mdl" )
+player_manager.AddValidHands( "Police Roro - Male #10", "models/roro/arms/c_arms_police_2.mdl", 0, "00000000" )
